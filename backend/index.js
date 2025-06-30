27c85f67c84da2ddee34375d7705c2f621360252
kavinduabc <piumalranepura2002@gmail.com>
2025-06-30 14:01:14 +0530

    done user model and start coding userController and user Router

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const mongoUrl = "mongodb+srv://admin:<db_password>@cluster0.paigr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const mongoUrl = "mongodb+srv://admin:root@cluster0.paigr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 mongoose.connect(mongoUrl).then(() =>
     console.log("MongoDB connection successfully")
 ).catch((err) =>
---
+++ backend/models/user.js
@@ -0,0 +1,25 @@
+import mongoose  from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username :{
+        type : String,
+        required : true
+    },
+    email :{
+        type : String,
+        required : true,
+        unique : true
+    },
+    password : {
+        type : String,
+        required : true
+    },
+    customer : {
+        type : String,
+        required : true,
+        default : "customer"   
+    }
+})
+
+const user = mongoose.model("Users",userSchema);
+export default user;
